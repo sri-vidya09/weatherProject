8e6e13067372e4fb6745237c5240a9b8a16f2f32
sri vidya <86715568+sri-vidya09@users.noreply.github.com>
2023-05-14 21:20:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
   
-const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={}`;
+const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
 
 
 async function getWeatherByLocation(city){
